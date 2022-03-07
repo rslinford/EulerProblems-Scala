9ce40cf832c8dd462e8d84307521dfcab686615f
Scott <scott@linfords.com>
2022-03-07 13:29:01 -0800

    Project files. Problem 2 correct, naive implementation only

---
+++ src/EulerProblem002.scala
@@ -0,0 +1,13 @@
+object EulerProblem001 {
+  def sumDivisibleBy(n: Int, target: Int): Int = {
+    var p = target / n
+    return n * (p * (p + 1)) / 2
+  }
+
+  def main(args: Array[String]) = {
+    var target = 999
+    var answer = (sumDivisibleBy(3, target) + sumDivisibleBy(5, target) - sumDivisibleBy(15, target))
+    println("Answer Euler 001: " + answer)
+  }
+}
+
