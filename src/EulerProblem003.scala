642e87f81738208767a3cce83977dd63e1584f21
Scott <scott@linfords.com>
2022-03-07 15:53:29 -0800

    Stubbed in. Ready to solve.

--- src/EulerProblem003.scala
+++ src/EulerProblem003.scala
@@ -1,13 +1,18 @@
 object EulerProblem003 {
-  def sumDivisibleBy(n: Int, target: Int): Int = {
-    var p = target / n
-    return n * (p * (p + 1)) / 2
-  }
 
+  // What is the largest prime factor of the number 600851475143 ?
   def main(args: Array[String]) = {
-    var target = 999
-    var answer = (sumDivisibleBy(3, target) + sumDivisibleBy(5, target) - sumDivisibleBy(15, target))
-    println("Answer Euler 001: " + answer)
+    var target = 600851475143L
+    var answer = 0
+    var i = 0L
+    while (answer == 0) {
+      i += 1;
+      println("(" + i + ") ")
+      if (i == 100) {
+        answer = -1
+      }
+    }
+    println("6857 Answer Euler 001: " + answer)
   }
 }
 
