359c563dc66ff9166d1615e478f5376937496f9e
Scott <scott@linfords.com>
2022-03-08 00:24:06 -0800

    Stubbed in. Ready to solve.

--- src/EulerProblem003.scala
+++ src/EulerProblem003.scala
@@ -1,15 +1,50 @@
 object EulerProblem003 {
 
+  def isPrimeUglyVersionRecursive(n:Long): Boolean = {
+    var p = 3L
+    if (n == 1) {
+      return true
+    } else if (n == 0) {
+      return false
+    }
+
+    if (n % 2 == 0) {
+      return false
+    }
+
+    return isPrimeUglyVersionRecursive(n%2);
+  }
+
   // What is the largest prime factor of the number 600851475143 ?
   def main(args: Array[String]) = {
     var target = 600851475143L
-    var answer = 0
+    var answer = 0L
     var i = 0L
+    var isFactor = false
+    var isPrime = false;
+
+    i = target / 2 - 1 // half target to count backwards
+//    i = 10000L
+    answer = 0L
     while (answer == 0) {
-      i += 1;
-      println("(" + i + ") ")
-      if (i == 100) {
+      i -= 1;
+      isFactor = target % i == 0
+      if (i % 10000000001L == 0) {
+        println("(" + i + ") isFactor(" + isFactor + ") isPrime(" + isPrimeUglyVersionRecursive(i) + ")")
+      }
+      if (isFactor) {
+        println("(" + i + ") isFactor(" + isFactor + ") isPrime(" + isPrimeUglyVersionRecursive(i) + ")")
+        if (isPrimeUglyVersionRecursive(i)) {
+          answer = i
+        }
+      }
+
+      // Bail. To be removed.
+      if (i < 3) {
         answer = -1
+      } else if (answer==0 && isFactor && isPrimeUglyVersionRecursive(i)) {
+        println("Burp")
+        answer = i
       }
     }
     println("6857 Answer Euler 001: " + answer)
