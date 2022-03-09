666388163b7a9d3de4c3fc372f203c1817c8524f
Scott <scott@linfords.com>
2022-03-08 19:19:27 -0800

    Problem 004 stubbed in

---
+++ src/EulerProblem004.scala
@@ -0,0 +1,31 @@
+object EulerProblem004 {
+
+  def testAnswer(anAnswer: Long): Unit = {
+    var knownAnswer = 906609
+    println("anAnswer(" + anAnswer + ") knownAnswer(" + knownAnswer + ")")
+    if (anAnswer == knownAnswer) {
+      println("Yay!")
+    }
+    else {
+      println("Rats!")
+    }
+  }
+
+  /*
+    A palindromic number reads the same both ways. The largest palindrome
+    made from the product of two 2-digit numbers is 9009 = 91 × 99.  Find
+    the largest palindrome made from the product of two 3-digit numbers.
+   */
+  def main(args: Array[String]) = {
+    var answer = 0
+    var i = 0
+
+    i = 0;
+    answer = 0
+    while (answer == 0) {
+      answer = 1234;
+    }
+    testAnswer(answer)
+  }
+}
+
