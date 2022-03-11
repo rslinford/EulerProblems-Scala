eb41f2c71c35164303ea8ab545d04af6709a8af9
Scott <scott@linfords.com>
2022-03-11 06:58:38 -0800

    isPrimeUglyVersionRecursive had bug for n=2 and n=1

--- src/EulerProblem007.scala
+++ src/EulerProblem007.scala
@@ -1,10 +1,14 @@
+import scala.annotation.tailrec
+
 object EulerProblem007 {
   /*
+   By listing the first six prime numbers: 2, 3, 5, 7, 11, and 13,
+   we can see that the 6th prime is 13. What is the 10 001st prime number?
 
-  Answer:  xxx
+   Answer:  104743
   */
   def testAnswer(anAnswer: Long): Unit = {
-    val knownAnswer = -1
+    val knownAnswer = 104743
     println("anAnswer(" + anAnswer + ") knownAnswer(" + knownAnswer + ")")
     if (anAnswer == knownAnswer) {
       println("Yay!")
@@ -14,20 +18,38 @@ object EulerProblem007 {
     }
   }
 
-  def isSomeTest(n: Int): Boolean = {
-    var isDone = false
-    var isPass = false
-    while (!isDone) {
-      isDone = true
-      isPass = true
+  @tailrec
+  def isPrimeUglyVersionRecursive(n:Long): Boolean = {
+    var p = 3L
+    if (n == 1 || n == 0) {
+      return false
+    } else if (n == 2) {
+      return true
     }
-    isPass
+
+    if (n % 2 == 0) {
+      return false
+    }
+
+    isPrimeUglyVersionRecursive(n%2)
   }
 
   def main(args: Array[String]): Unit = {
     var answer = 0
     var i = 0
-    answer = 123
+
+    var primeCount = 0
+    var candidatePrime = 0
+
+    while (primeCount < 7) {
+      candidatePrime += 1
+      if (isPrimeUglyVersionRecursive(candidatePrime)) {
+        primeCount += 1
+        println("primeCount(" + primeCount + ") candidatePrime(" + candidatePrime + ")")
+      }
+    }
+    answer = candidatePrime
+
     testAnswer(answer)
   }
 
