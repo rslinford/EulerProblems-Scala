3e93e3cbd604b7aba72f20fd055afaf89114fa87
Scott <scott@linfords.com>
2022-03-11 23:51:40 -0800

    Problem 010 completed. Ugly version takes several hours to finish. Oh well, moving on.

--- src/EulerProblem010.scala
+++ src/EulerProblem010.scala
@@ -1,5 +1,3 @@
-import scala.annotation.tailrec
-
 object EulerProblem010 {
   /*
     The sum of the primes below 10 is 2 + 3 + 5 + 7 = 17.
@@ -18,9 +16,66 @@ object EulerProblem010 {
     }
   }
 
-  def main(args: Array[String]): Unit = {
-    var answer = 0L
-    testAnswer(answer)
+  def isPrimeUglyVersionHelper(n:Long): Boolean = {
+    var potentialFactor = n
+    while (potentialFactor > 2) {
+      potentialFactor -= 1
+      // Return false if factor is found
+      if (n % potentialFactor == 0) {
+        // A factor was found therefore not prime.
+        return false
+      }
+    }
+    // No factors found. It's prime.
+    true
   }
 
+  def isPrimeUglyVersion(n:Long): Boolean = {
+    var p = 0L
+    var isPrime = false
+
+    if (n == 1 || n == 0) {
+      return false
+    } else if (n == 2 || n == 3) {
+      return true
+    }
+
+    // Test is even
+    if (n % 2 == 0) {
+      return false
+    }
+
+    isPrime = isPrimeUglyVersionHelper(n)
+    if (isPrime) {
+      return true
+    }
+
+    if (isPrime) {
+      return true
+    }
+
+    isPrime
+  }
+
+  def main(args: Array[String]): Unit = {
+    var answer = 0L
+    var sumOfPrimes = 0L
+    var isPrime = false
+    var primeCount = 0
+    val ALL_PRIMES_BELOW = 2000000L
+
+    for (potentialPrime <- 2L to ALL_PRIMES_BELOW-1L) {
+      isPrime = isPrimeUglyVersion(potentialPrime)
+      if (isPrime) {
+        primeCount += 1
+        sumOfPrimes += potentialPrime
+        if (primeCount%1000 == 0) {
+          println("potentialPrime(" + potentialPrime + ") primeCount(" + primeCount + ") sumOfPrimes(" + sumOfPrimes + ")")
+        }
+      }
+    }
+
+    answer = sumOfPrimes
+    testAnswer(answer)
+  }
 }
