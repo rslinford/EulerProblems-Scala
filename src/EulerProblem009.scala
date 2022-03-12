1807a8c411038c67a2f513ac77ef28f3ddb02285
Scott <scott@linfords.com>
2022-03-11 21:30:08 -0800

    Problem 009 stubbed in

--- src/EulerProblem009.scala
+++ src/EulerProblem009.scala
@@ -5,10 +5,7 @@ object EulerProblem009 {
     A Pythagorean triplet is a set of three natural numbers, a < b < c, for which,
        a2 + b2 = c2
     For example:
-       3^2 + 4^2
-       9 + 16
-       25
-       5^2
+       3^2 + 4^2 = 5^2
 
     There exists exactly one Pythagorean triplet for which a + b + c = 1000.
     Find the product abc.
@@ -22,14 +19,49 @@ object EulerProblem009 {
       println("Yay!")
     }
     else {
-      println("Rats!")
+      println("Dang-it.")
     }
   }
 
-  def main(args: Array[String]): Unit = {
-    var answer = 0L
+  def isATripletTest(a:Int, b:Int, c:Int): Boolean = {
+    a*a + b*b == c*c
+  }
 
-    answer = -1
+  def isTheTripletTest(a:Int, b:Int, c:Int): Boolean = {
+    a + b + c == 1000
+  }
+
+  def main(args: Array[String]): Unit = {
+    val TARGET_SUM = 1000
+    var answer = 0L
+    var a = 1
+    var b = 2
+    var c = 3
+    var isATriplet = false
+    var isTheTriplet = false
+
+    while(a <= TARGET_SUM && answer == 0) {
+      isATriplet = isATripletTest(a,b,c)
+      if (isATriplet) {
+        isTheTriplet = isTheTripletTest(a,b,c)
+        if (isTheTriplet) {
+          answer = a * b * c
+          println("a(" + a + ") b(" + b + ") c(" + c + ") answer(" + answer + ")")
+        }
+      }
+
+      // Advance the 3 counters
+      c += 1
+      if (c >= TARGET_SUM) {
+        b += 1
+        c = b + 1
+        if (b >= TARGET_SUM) {
+          a += 1
+          b = a + 1
+          c = b + 1
+        }
+      }
+    }
     testAnswer(answer)
   }
 
